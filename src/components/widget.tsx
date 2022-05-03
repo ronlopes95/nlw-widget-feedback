2dfed42829f234c98edda9fde5a13ab5506b7b44
Ronaldo Lopes <ronaldofslopes@gmail.com>
2022-05-03 10:06:47 -0300

    commit secundario

--- src/components/widget.tsx
+++ src/components/widget.tsx
@@ -5,7 +5,7 @@ import { Popover } from '@headlessui/react';
 export function Widget() {
     return (
         <Popover className="absolute bottom-5 right-5">
-            <Popover.Panel>vai toma no teu cu</Popover.Panel>
+            <Popover.Panel>Hello World</Popover.Panel>
 
             <Popover.Button  className="bg-brand-500 rounded-full px-3 h-12 text-white flex items-center group">
                 <ChatTeardropDots className="w-6 h-6"/>
